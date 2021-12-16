7ec043e093bbacecafcafc48125c8179fdc1efce
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2021-12-16 18:32:07 +0100

    Add clear all completed function

--- src/__test__/completed.test.js
+++ src/__test__/completed.test.js
@@ -1,11 +1,9 @@
 import LocalStorageMock from '../__mocks__/status.js';
 import { check, completedItem } from '../__mocks__/completed.js';
-// import completedItem from '../__mocks__/completed.js';
 
 describe('Simulate changing status', () => {
   const localStore = new LocalStorageMock();
   let toDoItems = [];
-  // localStore.setItem('storedToDoList', []);
 
   test('First item is not completed', () => {
     toDoItems = localStore.getItem('ToDoList');
